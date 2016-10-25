f83b327a41538c8f677e8caf74ba9bbaf90f3fba
xtgxiso <124960772@qq.com>
2016-10-25 10:52:11 +0800

    Create JsonRPC.php

---
+++ JsonRPC.php
@@ -0,0 +1,31 @@
+<?php
+class JsonRPC
+{
+    private $conn;
+
+    function __construct($host, $port) {
+        $this->conn = fsockopen($host, $port, $errno, $errstr, 3);
+        if (!$this->conn) {
+            return false;
+        }
+    }
+
+    public function Call($method, $params) {
+        if ( !$this->conn ) {
+            return false;
+        }
+        $err = fwrite($this->conn, json_encode(array(
+                'method' => $method,
+                'params' => array($params),
+                'id'     => 0,
+            ))."\n");
+        if ($err === false)
+            return false;
+        stream_set_timeout($this->conn, 0, 3000);
+        $line = fgets($this->conn);
+        if ($line === false) {
+            return NULL;
+        }
+        return json_decode($line,true);
+    }
+}
